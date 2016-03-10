11bf484d33512ace93be8bb88418a4ecb3fa8c9a
xWolki <alessandro_away2@hotmail.com>
2016-03-09 21:57:31 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -51,9 +51,9 @@
       chatLink: "https://rawgit.com/xWolki/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/xWolki/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
-      startupCap: 1, // 1-200
-      startupVolume: 0, // 0-100
-      startupEmoji: false, // true or false
+      startupCap: 20, // 1-200
+      startupVolume: 100, // 0-100
+      startupEmoji: true, // true or false
       autowoot: true,
       autoskip: false,
       smartSkip: true,
